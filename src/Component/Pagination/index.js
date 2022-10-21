30b3410fa9c73a4f7f3959cbfc2d3f6a60afc76b
akshay <akshay>
2022-10-21 13:35:29 +0530

    fix warning

--- src/Component/Pagination/index.js
+++ src/Component/Pagination/index.js
@@ -51,7 +51,7 @@ const Pagination = props => {
         }
 
         return (
-          <li
+          <li key={pageNumber}
             className={classnames('pagination-item', {
               selected: pageNumber === currentPage
             })}
